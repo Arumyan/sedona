0def7863f04089db679f35db265bb52071d81756
Arumyan <arumyan@example.com>
2018-01-24 16:15:24 +0300

    добавлено закрывание меню

--- source/js/main.js
+++ source/js/main.js
@@ -14,3 +14,28 @@ function initMap() {
 		icon: image
 	});
 }
+
+
+(function(){
+	$('.nav-btn-close').on('click', function(){
+		$('.nav-items').slideUp(300);
+		$('.nav-btn-close').hide();
+		$('.nav-btn-menu').show();
+	});
+
+	$('.nav-btn-menu').on('click', function(){
+		$('.nav-items').slideDown(300);
+		$('.nav-btn-close').show();
+		$('.nav-btn-menu').hide();
+	});
+
+	$(window).on('resize', function(){
+		var width = $(window).width()
+		if(width >= 768){
+			$('.nav-items').removeAttr('style');
+			$('.nav-btn-menu').hide();
+			$('.nav-btn-close').removeAttr('style');
+		}
+})
+
+})();
